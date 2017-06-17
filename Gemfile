f476465cccf8f156dedf73b40f9d2f59145d4b4b
Guilherme Solinscki <guilherme.slnk@gmail.com>
2017-06-17 01:38:34 -0300

    Setup test tools

--- Gemfile
+++ Gemfile
@@ -13,12 +13,19 @@ gem 'puma', '~> 3.7'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 3.6'
 end
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'faker', '~> 1.7', '>= 1.7.3'
+  gem 'factory_girl_rails', '~> 4.8'
+  gem 'database_cleaner', '~> 1.6', '>= 1.6.1'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/rails_helper.rb
@@ -0,0 +1,27 @@
+require 'database_cleaner'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  # Factory Girl config
+  config.include FactoryGirl::Syntax::Methods
+
+  # Database cleaner config
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around(:each) do |example|
+    Database.cleaning { example.run }
+  end
+end
