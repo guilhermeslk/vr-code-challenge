d83ff9239ea1858c31f247824d448b3aa6d90178
Guilherme Solinscki <guilherme.slnk@gmail.com>
2017-06-17 09:23:53 -0300

    Added model basic validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,10 @@
 class Property < ApplicationRecord
+  validates :beds, numericality: { greater_than_or_equal_to: 1,
+                                   less_than_or_equal_to: 5 }
+
+  validates :baths, numericality: { greater_than_or_equal_to: 1,
+                                    less_than_or_equal_to: 4 }
+
+  validates :square_meters, numericality: { greater_than_or_equal_to: 20,
+                                            less_than_or_equal_to: 240 }
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_numericality_of(:beds).is_greater_than_or_equal_to(1) }
+  it { should validate_numericality_of(:beds).is_less_than_or_equal_to(5) }
+
+  it { should validate_numericality_of(:baths).is_greater_than_or_equal_to(1) }
+  it { should validate_numericality_of(:baths).is_less_than_or_equal_to(4) }
+
+  it do
+    should validate_numericality_of(:square_meters)
+      .is_greater_than_or_equal_to(20)
+  end
+
+  it do
+    should validate_numericality_of(:square_meters)
+      .is_less_than_or_equal_to(240)
+  end
 end
