0633c7b231a9f888509b7eba508c876735700968
Guilherme Solinscki <guilherme.slnk@gmail.com>
2017-06-20 00:12:08 -0300

    Refactored seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,44 @@
-Province.create!(
-  name: 'Gode',
-  upper_left_boundary_x: 0,
-  upper_left_boundary_y: 1000,
-  bottom_right_boundary_x: 600,
-  bottom_right_boundary_y: 500
-)
-
-Province.create!(
-  name: 'Ruja',
-  upper_left_boundary_x: 400,
-  upper_left_boundary_y: 1000,
-  bottom_right_boundary_x: 1100,
-  bottom_right_boundary_y: 500
-)
-
-Province.create!(
-  name: 'Jaby',
-  upper_left_boundary_x: 1100,
-  upper_left_boundary_y: 1000,
-  bottom_right_boundary_x: 1400,
-  bottom_right_boundary_y: 500
-)
-
-Province.create!(
-  name: 'Scavy',
-  upper_left_boundary_x: 0,
-  upper_left_boundary_y: 500,
-  bottom_right_boundary_x: 600,
-  bottom_right_boundary_y: 0
-)
-
-Province.create!(
-  name: 'Groola',
-  upper_left_boundary_x: 600,
-  upper_left_boundary_y: 500,
-  bottom_right_boundary_x: 800,
-  bottom_right_boundary_y: 0
-)
-
-Province.create!(
-  name: 'Nova',
-  upper_left_boundary_x: 800,
-  upper_left_boundary_y: 500,
-  bottom_right_boundary_x: 1400,
-  bottom_right_boundary_y: 0
-)
+Province.create!([
+  {
+    name: 'Gode',
+    upper_left_boundary_x: 0,
+    upper_left_boundary_y: 1000,
+    bottom_right_boundary_x: 600,
+    bottom_right_boundary_y: 500
+  },
+  {
+    name: 'Ruja',
+    upper_left_boundary_x: 400,
+    upper_left_boundary_y: 1000,
+    bottom_right_boundary_x: 1100,
+    bottom_right_boundary_y: 500
+  },
+  {
+    name: 'Jaby',
+    upper_left_boundary_x: 1100,
+    upper_left_boundary_y: 1000,
+    bottom_right_boundary_x: 1400,
+    bottom_right_boundary_y: 500
+  },
+  {
+    name: 'Scavy',
+    upper_left_boundary_x: 0,
+    upper_left_boundary_y: 500,
+    bottom_right_boundary_x: 600,
+    bottom_right_boundary_y: 0
+  },
+  {
+    name: 'Groola',
+    upper_left_boundary_x: 600,
+    upper_left_boundary_y: 500,
+    bottom_right_boundary_x: 800,
+    bottom_right_boundary_y: 0
+  },
+  {
+    name: 'Nova',
+    upper_left_boundary_x: 800,
+    upper_left_boundary_y: 500,
+    bottom_right_boundary_x: 1400,
+    bottom_right_boundary_y: 0
+  }
+])
