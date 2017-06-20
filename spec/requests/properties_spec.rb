4ac75cbc500c1228e9701147e08ae956ef63181d
Guilherme Solinscki <guilherme.slnk@gmail.com>
2017-06-19 23:31:21 -0300

    Fixed spec to use faker

--- spec/requests/properties_spec.rb
+++ spec/requests/properties_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe 'Properties', type: :request do
     headers = { 'ACCEPT' => 'application/json' }
     params = {
       property: {
-        x: 222,
-        y: 444,
-        title: 'Imóvel código 1, com 5 quartos e 4 banheiros',
-        price: 1_250_000,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        beds: 4,
-        baths: 3,
-        square_meters: 240
+        x: Faker::Number.between(0, 1400),
+        y:  Faker::Number.between(0, 1000),
+        title: Faker::Lorem.sentence,
+        price: Faker::Number.decimal(8),
+        description: Faker::Lorem.paragraph,
+        beds: Faker::Number.between(1, 5),
+        baths: Faker::Number.between(1, 4),
+        square_meters: Faker::Number.between(20, 240)
       }
     }
 
