1d590ff4cb73830e57e9dde9896b434f47a1e755
Guilherme Solinscki <guilherme.slnk@gmail.com>
2017-06-19 23:19:53 -0300

    Removed unused method

--- app/models/province.rb
+++ app/models/province.rb
@@ -4,11 +4,4 @@ class Province < ApplicationRecord
 
   validates_presence_of :name, :upper_left_boundary_x, :upper_left_boundary_y,
                         :bottom_right_boundary_x, :bottom_right_boundary_y
-
-  def self.query_by_property_coordinates(x, y)
-    where("upper_left_boundary_x < :x AND
-           upper_left_boundary_y > :y AND
-           bottom_right_boundary_x > :x AND
-           bottom_right_boundary_y < :y", x: x, y: y)
-  end
 end
